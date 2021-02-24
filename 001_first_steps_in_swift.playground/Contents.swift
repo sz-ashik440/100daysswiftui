2b119517d11aa1c6cacda7bba1729484a9740d4d
sz ashik <szashik@szs-Mac-mini.local>
2021-02-25 01:56:29 +0600

    Day 001 done

---
+++ 001_first_steps_in_swift.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+var age = 38
+
+var population = 10_000_000
+
+
+var mutiStr1 = """
+This goes
+multiple
+lines
+"""
+
+
+var multiStr2 = """
+This goes \
+over multiple \
+but no newline
+"""
+
+var pi = 3.1416
+
+var awesome = true
+
+var score = 85
+
+var strScore = "Your score was \(score)"
+
+var results = "The result is: \(strScore)"
+
+let taylor = "Swift"
+
+let strPlayground = "Hello, playground"
+
+let album: String = "Reputation"
+
+let year: Int = 1990
+
+let height: Double = 1.79
+
+let taylorRocks: Bool = true
---
+++ README.md
@@ -0,0 +1,4 @@
+# 100 Days of SwiftUI
+
+Reference for [100 Days of SwiftUI](https://www.hackingwithswift.com/100/swiftui) Challenge.
+
